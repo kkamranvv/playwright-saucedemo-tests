a6d4546a3a1139a976609cc3e12a9b814d1264f0
kamik <musadirlikamran@gmail.com>
2025-10-24 23:53:53 +0200

    Update reporter configuration in playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
 
-  // ✅ Репортёры: HTML + Testomat.io
+  
   reporter: [
     ["html"],
     [
